f7300f4b69de3a5b8472001948ba34c4317758b7
Erick Martín del Campo Castrejón <erick.martindelcampo@jenkon.com>
2025-04-10 13:30:27 -0500

    Add Feature Management

--- learningapp/Program.cs
+++ learningapp/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.FeatureManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Configuration.AddAzureAppConfiguration("Endpoint=https://applicationconfig1000.azconfig.io;Id=T1M7;Secret=F1joyQwoV4YqVZw9Mtb1Xc653u3hpjy2y7oWez7CGlW0DxEhm5gLJQQJ99BDACYeBjFKgyCrAAABAZAC418i");
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(
+        "Endpoint=https://applicationconfig1000.azconfig.io;Id=T1M7;Secret=F1joyQwoV4YqVZw9Mtb1Xc653u3hpjy2y7oWez7CGlW0DxEhm5gLJQQJ99BDACYeBjFKgyCrAAABAZAC418i"
+    );
+    options.UseFeatureFlags();
+});
+
+builder.Services.AddFeatureManagement();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969f7e4b83101fa50ab202755fb8813cf524ae66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb766c64b66976311509e3f35a6d2886ed40b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
